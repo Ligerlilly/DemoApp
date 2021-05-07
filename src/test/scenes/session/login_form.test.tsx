c734e81e3a11282da92e636fe7ce675a299d36a9
Jason Lane <2266577+Ligerlilly@users.noreply.github.com>
2021-05-06 20:57:54 -0700

    Update test and remove manual linking

--- src/test/scenes/session/login_form.test.tsx
+++ src/test/scenes/session/login_form.test.tsx
@@ -4,27 +4,29 @@ import { shallow } from "enzyme";
 import LoginForm from "../../../scenes/session/components/login_form";
 
 describe("<LoginForm/>", () => {
-    it("it should call handleLogin when pressed", () => {
-        const mockHandleLogin = jest.fn();
-        const mockOnChangeText = (stateKey: string) => (text: string) => {
-            // no op
-        };
+    describe("Login button", () => {
+        it("it should call handleLogin when pressed", () => {
+            const mockHandleLogin = jest.fn();
+            const mockOnChangeText = (stateKey: string) => (text: string) => {
+                // no op
+            };
 
-        const wrapper = shallow(
-            <LoginForm
-                jobcoinAddress="Alice"
-                onChangeText={mockOnChangeText}
-                handleLogin={mockHandleLogin}
-            />
-        );
+            const wrapper = shallow(
+                <LoginForm
+                    jobcoinAddress="Alice"
+                    onChangeText={mockOnChangeText}
+                    handleLogin={mockHandleLogin}
+                />
+            );
 
-        const test = wrapper.find(TouchableOpacity);
-        const onPress = test.invoke("onPress");
-        if (onPress) {
-            onPress({} as GestureResponderEvent);
-        }
+            const test = wrapper.find(TouchableOpacity);
+            const onPress = test.invoke("onPress");
+            if (onPress) {
+                onPress({} as GestureResponderEvent);
+            }
 
-        expect(mockHandleLogin).toHaveBeenCalled();
+            expect(mockHandleLogin).toHaveBeenCalled();
+        });
     });
 });
 
